6d00fd998c2ee2b1352922fc513652b84d1814d4
unknown <sicome.a.s@gmail.com>
2024-01-10 19:33:47 +0300

    Added blank technical documentation.

--- Exider-Version-2.0.0/Controllers/WeatherForecastController.cs
+++ Exider-Version-2.0.0/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace Exider_Version_2._0._0.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
