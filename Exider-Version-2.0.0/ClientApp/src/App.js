48edeb48fe826d10edf9b5ee29c3762904a32595
unknown <sicome.a.s@gmail.com>
2024-01-24 19:43:24 +0300

    Added side panel

--- Exider-Version-2.0.0/ClientApp/src/App.js
+++ Exider-Version-2.0.0/ClientApp/src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
+import Layout from './global/components/layout/Layout';
 
 export default class App extends Component {
 
     render() {
 
         return (
-
-            <Routes>
-                {AppRoutes.map((route, index) => {
-                    const { element, ...rest } = route;
-                    return <Route key={index} {...rest} element={element} />;
-                })}
-            </Routes>
-
+            <Layout>
+                <Routes>
+                    {AppRoutes.map((route, index) => {
+                        const { element, ...rest } = route;
+                        return <Route key={index} {...rest} element={element} />;
+                    })}
+                </Routes>
+            </Layout>
         );
 
     }
---
+++ Exider-Version-2.0.0/ClientApp/src/global/components/layout/Layout.js
@@ -0,0 +1,18 @@
+﻿import React from 'react'
+import LeftPanel from '../left-panel/LeftPanel';
+import '../../css/fonts.css'
+import '../../css/colors.css'
+import '../../css/main.css'
+
+const Layout = ({ children }) => {
+
+    return (
+        <>
+            <LeftPanel />
+            {children}
+        </>
+    );
+
+};
+
+export default Layout;
