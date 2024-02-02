0e1cd17c8d125751ebd8ebdbc512b1b071e87842
unknown <sicome.a.s@gmail.com>
2024-02-02 22:04:39 +0300

    Added HtmlEncoder

---
+++ Exider.Services/External/EmailService/HtmlEncoder.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Exider.Services.External.EmailService
+{
+
+    public static class HtmlEncoder
+    {
+
+        private static readonly string _imagePattern = "<img.*?src=[\"']?(.*?)[\"']?\\s.*?>";
+
+        private static readonly Mutex _mutex = new Mutex();
+
+        public static string EncodeHtml(string html)
+        {
+
+            string result = Regex.Replace(html, _imagePattern, match =>
+            {
+                string imagePath = match.Groups[1].Value;
+                string base64String = GetImageBase64String(imagePath);
+                return $"<img src=\"data:image;base64,{base64String}\" />";
+            });
+
+            return result.Replace("\n", "");
+
+        }
+
+        public static void EncodeHtmlFromFile(string path, string outputPath)
+        {
+
+            if (path != null)
+            {
+
+                File.WriteAllLines(outputPath, new string[] {
+
+                    EncodeHtml(string.Join("", File.ReadAllLines(path)))
+
+                });
+
+            }
+
+        }
+
+        private static string GetImageBase64String(string imagePath)
+        {
+
+            if (string.IsNullOrEmpty(imagePath))
+            {
+                throw new ArgumentException(imagePath, nameof(imagePath));
+            }
+
+            _mutex.WaitOne();
+
+            try
+            {
+                return Convert.ToBase64String(File.ReadAllBytes(imagePath));
+            }
+
+            finally
+            {
+                _mutex.ReleaseMutex();
+            }
+
+        }
+
+    }
+
+}
---
+++ Tests/TestEmailService.cs
@@ -0,0 +1,24 @@
+﻿using Exider.Services.External.EmailService;
+
+namespace Exider.Tests
+{
+
+    [TestClass]
+    internal class TestEmailService
+    {
+
+        [TestMethod]
+        public void TestHtmlEncoder()
+        {
+
+            string inputHtmlFilePath = "D:\\[1] Exider Projects\\Exider-Version-2.0.0\\Exider.Services\\External\\EmailService\\Emails\\confirm-register.html";
+            string outputHtmlFilePath = "D:\\[1] Exider Projects\\Exider-Version-2.0.0\\Exider.Services\\External\\EmailService\\Emails\\confirm-register-min.html";
+
+            HtmlEncoder.EncodeHtmlFromFile(inputHtmlFilePath, outputHtmlFilePath);
+            Assert.IsTrue(File.Exists(outputHtmlFilePath));
+
+        }
+
+    }
+
+}
