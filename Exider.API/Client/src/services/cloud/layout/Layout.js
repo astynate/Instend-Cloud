b428db9ed8354273402c36f2b440933b04f85ae1
Святослав Андреев <sicome.a.s@gmail.com>
2024-02-08 21:36:10 +0300

    Not set

--- Exider.API/Client/src/services/cloud/layout/Layout.js
+++ Exider.API/Client/src/services/cloud/layout/Layout.js
@@ -1,7 +1,7 @@
 ﻿import React from 'react'
 import { useState } from 'react';
-import LeftPanel from '../left-panel/LeftPanel';
-import Loader from '../loader/Loader';
+import LeftPanel from './left-panel/LeftPanel';
+import Loader from '../../../global/components/loader/Loader';
 import '../../css/fonts.css'
 import '../../css/colors.css'
 import '../../css/main.css'
