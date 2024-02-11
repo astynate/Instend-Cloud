0904036d3503de815a023e4462bc120b89bc6e17
unknown <sicome.a.s@gmail.com>
2024-02-11 18:43:53 +0300

    Error

---
+++ Exider.API/Client/src/services/accounts/processes/Registaration.js
@@ -0,0 +1,30 @@
+﻿import React, { useState, createContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const UserContext = createContext();
+
+const Registration = ({ children }) => {
+
+    const defaultUser = {
+
+        name: '',
+        surname: '',
+        nickname: '',
+        email: '',
+        password: ''
+
+    }
+
+    const [user, setUser] = useState(defaultUser);
+
+    return (
+
+        <UserContext.Provider value={user}>
+            {children}
+        </UserContext.Provider>
+
+    );
+
+}
+
+export default UserContext;
