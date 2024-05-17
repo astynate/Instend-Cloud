90de23296ed4549e74184c6d84e04e814202a7e7
Святослав Андреев <sicome.a.s@gmail.com>
2024-05-17 18:08:11 +0300

    Added music

---
+++ Exider.Services/External/FileService/MusicService.cs
@@ -0,0 +1,31 @@
+﻿using CSharpFunctionalExtensions;
+using static TagLib.File;
+
+namespace Exider.Services.External.FileService
+{
+    public class MusicService
+    {
+        public async Task<Result> AddSong(byte[] file)
+        {
+            using (var stream = new MemoryStream(file))
+            {
+                var tagLibFile = TagLib.File.Create(new Fi);
+
+                string title = file.Tag.Title;
+                string[] artists = file.Tag.Performers;
+                string album = file.Tag.Album;
+                uint year = file.Tag.Year;
+                string[] genres = file.Tag.Genres;
+
+                // Выводим метаданные
+                Console.WriteLine("Title: " + title);
+                Console.WriteLine("Artists: " + string.Join(", ", artists));
+                Console.WriteLine("Album: " + album);
+                Console.WriteLine("Year: " + year);
+                Console.WriteLine("Genres: " + string.Join(", ", genres));
+            }
+
+            return Result.Success();
+        }
+    }
+}
