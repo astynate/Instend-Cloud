30d7415234840d7e25382c4f5c7d7511acd945d6
unknown <sicome.a.s@gmail.com>
2024-05-20 18:19:08 +0300

    !

--- Exider.API/Client/src/services/cloud/pages/music/pages/Songs/Songs.js
+++ Exider.API/Client/src/services/cloud/pages/music/pages/Songs/Songs.js
@@ -5,11 +5,19 @@ import SongList from '../../widgets/song-list/SongList';
 import storageState from '../../../../../../states/storage-state';
 import FileAPI from '../../../../api/FileAPI';
 
-const Songs = () => {
+const Songs = (props) => {
     return (
         <div className={styles.songs}>
             <SongInformation />
             <SongList songs={storageState.GetSelectionByType(FileAPI.musicTypes)} />
+            <Scroll
+                scroll={props.scroll}
+                isHasMore={galleryState.albums[params.id].hasMore}
+                count={galleryState.albums[params.id].photos.length}
+                callback={() => {
+                    galleryState.GetAlbumPhotos(params.id);
+                }}
+            />
         </div>
     );
 }
